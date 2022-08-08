7469a55b8bcda8b25cd77be15e85bc72b89cb24a
soprano95 <sacko202@gmail.com>
2022-08-08 22:33:24 +0000

    maison

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :branch, "main"
 
  set :passenger_restart_with_touch, true
 
- set :rbenv_path, '/home/siliconmali/.rbenv
+ set :rbenv_path, '$HOME/siliconmali/.rbenv'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -28,7 +28,7 @@ set :branch, "main"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+# append :linked_files, "config/database.yml", '#config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
