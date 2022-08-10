ce81cd855c7a43a1d65ef1bab98a5cc8ac3c8f46
soprano95 <sacko202@gmail.com>
2022-08-10 18:06:14 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  get 'pages/index'
+  
   get 'pages/about'
   get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
