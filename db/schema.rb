61c7e09d87df511f8648cfb5011cff25b1096924
soprano95 <sacko202@gmail.com>
2022-08-11 15:27:11 +0000

    relationship between user and blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_142536) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_152611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,12 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_142536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "views", default: 0
-  end
-
-  create_table "titles", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_142536) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
