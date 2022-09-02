b465b5efd978a44799be0f19897b33cb783b8e29
soprano95 <sacko202@gmail.com>
2022-09-02 16:44:21 +0000

    added image to blog through active_storage

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,6 +6,9 @@ class Blog < ApplicationRecord
 	has_one :content, class_name: 'ActionText::RichText', as: :record, dependent: :destroy
 	has_many :comments, dependent: :destroy
 
+	has_one_attached :image
+	 has_one_attached :pictures
+
 	has_noticed_notifications model_name: 'Notification'
     has_many :notifications, through: :user
 
